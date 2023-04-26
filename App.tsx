cf9ec025a6e34d7603227686f291bfba546f8dab
Mchandless <68225685+Mchandless@users.noreply.github.com>
2023-04-26 17:15:27 +0800

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Staasjkdhaljdz!</h1>
+      <h2>1829731289461984</h2>
+      <p1>Start edi:)</p1>
+      
+      
+      </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-vuokfm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-vuokfm)
